64f96a22569d04b151460594a4314953703df23a
Mahesh Vattipalli <maheshchary660@gmail.com>
2025-02-05 13:47:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 async function generateDocument(type) {
-    let userInputs = {};
-
+    let userInputs = {
+        name: "",
+        receiver: "",
+        purpose: "",
+        landlord: "",
+        tenant: "",
+        property: "",
+        rentAmount: ""
+    };
+    
     if (type === "noc") {
         userInputs = {
+            type: "noc",
             name: document.getElementById("nocName").value,
             receiver: document.getElementById("nocReceiver").value,
             purpose: document.getElementById("nocPurpose").value
         };
     } else if (type === "rental") {
         userInputs = {
+            type: "rental",
             landlord: document.getElementById("landlord").value,
             tenant: document.getElementById("tenant").value,
             property: document.getElementById("property").value,
             rentAmount: document.getElementById("rentAmount").value
         };
     }
-
-    const response = await fetch("https://hook.make.com/YOUR_WEBHOOK_URL", {
+    const response = await fetch("https://hook.eu2.make.com/e4k36mlugsisxq5arp9vxr9l9tr73jh0", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ documentType: type, userInputs })
